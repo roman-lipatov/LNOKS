b34c566d29b4a7ec758e5e6166de4bd64633fcfa
Roman Lipatov <rlipatov96@gmail.com>
2022-08-26 15:22:13 +0300

    Updates

--- test/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom';
-import 'bulma/css/bulma.css';
-import { App } from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
--- test/src/types/MovieData.ts
+++
@@ -1,6 +0,0 @@
-export interface MovieData {
-  Poster: string;
-  Title: string;
-  Plot: string;
-  imdbID: string;
-}
